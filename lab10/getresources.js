67760c28d9d30bc04084d7c451649c9ff07ce755
Ryan McConnell <ryanmcconnell.rm@gmail.com>
2018-03-02 20:50:18 +0000

    Changes made to brackets.

--- lab10/getresources.js
+++ lab10/getresources.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("#retrieve-resources").click(function() {
     var displayResources = $('#display-resources');
-    displayResources.text('Loading data from JSON....');
+    displayResources.text('Loading data from JSON source....');
     $.ajax({
       type: "GET",
       url: "resources.json",
@@ -11,13 +11,12 @@ $(document).ready(function() {
         var output= "<table><thead><tr><th>Name</th><th>Provider</th><th>URL</th></thead><tbody>";
         for (var i in result)
         {
-          output+=
-          "<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" + result[i].url + "</td></tr>";
+          output+="<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" + result[i].url + "</td></tr>";
         }
         output +="</tbody></table>";
         displayResources.html(output);
         $("table").addClass("table");
-      });
+      }
     });
   });
 });
